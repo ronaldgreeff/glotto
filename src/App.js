cb562fa372209566ee9d5cf25f2d1f5b40430d3c
ronald greeff <ron@donrondadon.com>
2020-09-10 20:22:27 +0100

    material ui setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'fontsource-roboto';
 import './App.css';
+import Button from '@material-ui/core/Button';
+
+
+function Square(props) {
+  return (
+    <Button className="square">
+      {props.value}
+    </Button>
+    );
+}
+
+function Row(props) {
+  return (
+    null
+    );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Square value="dick" />
   );
 }
 
