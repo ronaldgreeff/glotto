cd9e732676d82952836bf77c68be6fa237b0b97d
Ron G <ronald.greeff@gmail.com>
2020-09-14 09:53:23 +0100

    App func->class

--- src/App.js
+++ src/App.js
@@ -6,18 +6,31 @@ import Container from '@material-ui/core/Container';
 import { spacing } from '@material-ui/system';
 import Box from '@material-ui/core/Box';
 
+class App extends React.Component {
 
-function App() {
-  return (
-    <div className="App">
-      <AppBar />
-      <Box pt={2}>
-        <Container >
-          <TicketGrid />
-        </Container>
-      </Box>
-    </div>
-  );
+  constructor(props) {
+    super(props);
+    this.state = {
+      ticketData: [{
+        x: 7,
+        y: 7,
+        n: [1,2,3,4,5,6,7]
+      }]
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <AppBar />
+        <Box pt={2}>
+          <Container >
+            <TicketGrid />
+          </Container>
+        </Box>
+      </div>
+    );
+  }
 }
 
 export default App;
